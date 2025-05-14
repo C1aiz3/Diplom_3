9933b283b35bdae36811ab53a64dc650b1b7d403
Pavel Tishenko <pavelclaize@gmail.com>
2025-05-14 21:29:26 +0300

    Add changed data.py

--- src/data.py
+++ src/data.py
@@ -1,15 +1,24 @@
-from faker import Faker
-
-
 URL = 'https://stellarburgers.nomoreparties.site/'
 EMAIL = 'pavel_tishenko_19_999@mail.ru'
 PASSWORD = '1234567890'
 
-fake = Faker('ru_RU')
+
+class URLS():
+    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
+    FEED_PAGE_URL = 'https://stellarburgers.nomoreparties.site/feed'
+    INGREDIENT_URL = 'https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa6d'
+    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
+    ORDER_HISTORY_URL = 'https://stellarburgers.nomoreparties.site/account/order-history'
+    LOGIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login'
+    FORGOT_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
+    RESET_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/reset-password'
 
 
-class FakeData():
 
-    @staticmethod
-    def f_mail():
-        return fake.email()
+#@allure.step('Проверка отображения заказа в ленте заказов')
+    #def order_feed_displayed(self, number):
+        #order_locator = (By.XPATH, f'//p[contains(@class, "OrderFeed_number") and text()="{number}"]')
+        #return self.element_displayed(order_locator)
+
+
+
