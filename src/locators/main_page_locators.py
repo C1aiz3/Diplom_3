a35affe753d241000f4323565add4b387a63c741
Pavel Tishenko <pavelclaize@gmail.com>
2025-05-15 14:36:44 +0300

    Add fixed main_page_locators.py

--- src/locators/main_page_locators.py
+++ src/locators/main_page_locators.py
@@ -10,6 +10,5 @@ class MainPageLocators:
     INGREDIENT_MODAL = By.XPATH, '//div[contains(@class, "pt-10 pb-15")]' #модальное окно ингредиента
     INGREDIENT_MODAL_CLOSE = By.XPATH, '//section[@class="Modal_modal_opened__3ISw4 Modal_modal__P3_V5"]//button' #кнопка закрытия модального окна ингредиента
     ORDER_MODAL = By.XPATH, '//div/section/div[1]' #модальное окно заказа
-    CLOSE_ORDER_MODAL = By.XPATH, '//button[contains(@class, "close_modified")]' #кнопка закрытия модального окна заказа
+    CLOSE_ORDER_MODAL = By.XPATH, '//button[contains(@class, "close_modified")]'  # кнопка закрытия модального окна заказа
     MAKE_ORDER_BUTTON = By.XPATH, '//main/section//button' #кнопка Оформить заказ
-    ING_MODAL_TITLE = By.XPATH, '//h2[contains(@class, "title_modified")]'
